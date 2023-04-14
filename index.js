3e96299485987ef66110d630ac0ead6ab7437870
MorriganH <morriganhennessy@gmail.com>
2023-04-14 15:51:33 -0400

    adds check if balance is high enough for a withdrawl

--- index.js
+++ index.js
@@ -27,21 +27,37 @@ class Transaction {
   }
 
   commit() {
-    this.time = new Date();
-    this.account.addTransaction(this);
+    if (this.isAllowed()) {
+      this.time = new Date();
+      this.account.addTransaction(this);
+      return true;
+    }
+    return false;
   }
 
 }
 
 class Withdrawal extends Transaction {
 
+  isAllowed() {
+    if (this.account.balance >= this.amount) {
+      return true;
+    }
+    return false;
+  }
+
   get value() {
     return -this.amount;
   }
 
+
 }
 
 class Deposit extends Transaction {
+
+  isAllowed() {
+    return true;
+  }
 
   get value() {
     return this.amount;
@@ -57,16 +73,22 @@ class Deposit extends Transaction {
 
 const myAccount = new Account('snow-partol');
 
-t1 = new Withdrawal(50.25, myAccount);
+t1 = new Withdrawal(1, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99, myAccount);
+t2 = new Deposit(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-t3 = new Deposit(120.00, myAccount);
+t3 = new Withdrawal(9.99, myAccount);
 t3.commit();
-console.log('Transattion 3:', t3);
+console.log('Transaction 3:', t3);
+
+t4 = new Deposit(120.00, myAccount);
+t4.commit();
+console.log('Transattion 4:', t4);
 
 console.log('Balance:', myAccount.balance);
+
+console.log('Transaction History:', myAccount.transactions);
