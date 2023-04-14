6a6ee553662dbabb7ca1209994300f20d92768d9
MorriganH <morriganhennessy@gmail.com>
2023-04-14 15:16:25 -0400

    adds Account class, Transaction class with Withdrawal and Deposit subclasses

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
 
-  constructor(amount) {
+  get balance() {
+    let total = 0;
+    for (let transaction of this.transactions) {
+      total += transaction.value;
+    }
+    return total;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+
+}
+
+class Transaction {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.time = new Date();
+    this.account.addTransaction(this);
+  }
+
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
   }
 
 }
@@ -18,12 +55,18 @@ class Withdrawal {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('snow-partol');
+
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transattion 3:', t3);
+
+console.log('Balance:', myAccount.balance);
